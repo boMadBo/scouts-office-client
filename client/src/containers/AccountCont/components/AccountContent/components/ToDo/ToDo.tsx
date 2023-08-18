2bbdcfbfbad02f4341bd92270af6601ab995312c
Boris Berdnikov <bb@MacBook-Bo.local>
2023-08-18 22:37:43 +0700

    todo on server

--- client/src/containers/AccountCont/components/AccountContent/components/ToDo/ToDo.tsx
+++ client/src/containers/AccountCont/components/AccountContent/components/ToDo/ToDo.tsx
@@ -1,52 +1,35 @@
+import { ITasks } from '@/interfaces';
+import { tasksAPI } from '@/store/services/TasksService';
 import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ListItem from './ListItem';
 import TaskField from './TaskField';
 import styles from './ToDo.module.scss';
 
-const tasksData = [
-  { text: 'Find a striker', completed: true },
-  { text: "Extend Drogba's contract", completed: false },
-];
-
 const ToDo = () => {
-  const [tasks, setTasks] = useState([...tasksData]);
+  const { data: tasks } = tasksAPI.useGetTasksQuery();
+  const [createTasks] = tasksAPI.useCreateTasksMutation();
+  const [deleteTasks] = tasksAPI.useDeleteTasksMutation();
+  const [completedTasks] = tasksAPI.useCompletedTasksMutation();
   const [text, setText] = useState('');
   const { t } = useTranslation();
-
-  const onToggleCompleted = useCallback(
-    (index: number) => {
-      setTasks((prevTasks) =>
-        prevTasks.map((task, curIdx) => (index === curIdx ? { ...task, completed: !task.completed } : task)),
-      );
-    },
-    [setTasks],
-  );
-
-  const onRemoveTask = useCallback(
-    (index: number) => {
-      setTasks((prevTasks) => prevTasks.filter((_, curIdx) => index !== curIdx));
-    },
-    [setTasks],
-  );
-
-  const onAddTask = useCallback(
-    (text: string) => {
-      setTasks((prevTasks) => [...prevTasks, { text, completed: false }]);
-    },
-    [setTasks],
-  );
-
-  // field //
 
   const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.currentTarget.value;
     setText(value);
   }, []);
 
+  const onAddTask = async (values: ITasks) => {
+    await createTasks({
+      text: values.text,
+      completed: false,
+    });
+    setText('');
+  };
+
   const addTask = useCallback(() => {
     if (text) {
-      onAddTask(text);
+      onAddTask({ text, completed: false });
       setText('');
     }
   }, [onAddTask, text]);
@@ -57,23 +40,41 @@ const ToDo = () => {
         addTask();
       }
     },
-    [addTask],
+    [addTask]
   );
 
-  // list //
-
-  const toggleCompleted = useCallback(
-    (index: number) => {
-      onToggleCompleted(index);
-    },
-    [onToggleCompleted],
-  );
+  const onRemoveTask = async (taskId: string) => {
+    try {
+      await deleteTasks({ _id: taskId });
+    } catch (error) {
+      console.error('Error removing task:', error);
+    }
+  };
 
   const removeTask = useCallback(
-    (index: number) => {
-      onRemoveTask(index);
+    (id: string | undefined) => {
+      if (id !== undefined) {
+        onRemoveTask(id);
+      }
     },
-    [onRemoveTask],
+    [onRemoveTask]
+  );
+
+  const onToggleCompleted = async (taskId: string, completed: boolean) => {
+    try {
+      await completedTasks({ _id: taskId, completed: completed });
+    } catch (error) {
+      console.error('Error toggling task completion:', error);
+    }
+  };
+
+  const toggleCompleted = useCallback(
+    (id: string | undefined, completed: boolean) => {
+      if (id !== undefined) {
+        onToggleCompleted(id, completed);
+      }
+    },
+    [onToggleCompleted]
   );
 
   return (
@@ -81,10 +82,10 @@ const ToDo = () => {
       <div className={styles.container}>
         <h4 className={styles.text}>{t('Tasks list')}</h4>
         <TaskField text={text} handleInputChange={handleInputChange} addTask={addTask} handleKeyUp={handleKeyUp} />
-        {tasks.map((task, index) => (
+        {tasks?.map(task => (
           <ListItem
-            key={index}
-            index={index}
+            key={task._id}
+            id={task._id}
             completed={task.completed}
             text={task.text}
             toggleCompleted={toggleCompleted}
---
+++ client/src/store/services/TasksService.ts
@@ -0,0 +1,40 @@
+import { baseQueryWithAuth } from '@/api/baseQueryWithAuth';
+import { ITasks } from '@/interfaces';
+import { createApi } from '@reduxjs/toolkit/dist/query/react';
+
+export const tasksAPI = createApi({
+  reducerPath: 'tasksAPI',
+  baseQuery: baseQueryWithAuth,
+  tagTypes: ['Tasks'],
+  endpoints: build => ({
+    getTasks: build.query<ITasks[], void>({
+      query: () => ({
+        url: '/tasks',
+      }),
+      providesTags: (result, error) => [{ type: 'Tasks', result, error }],
+    }),
+    createTasks: build.mutation<ITasks[], ITasks>({
+      query: task => ({
+        url: `/tasks`,
+        method: 'POST',
+        body: task,
+      }),
+      invalidatesTags: ['Tasks'],
+    }),
+    deleteTasks: build.mutation<ITasks, { _id: string }>({
+      query: (arg: { _id: string }) => ({
+        url: `/tasks/${arg._id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Tasks'],
+    }),
+    completedTasks: build.mutation<ITasks, { completed: boolean; _id: string }>({
+      query: (arg: { completed: boolean; _id: string }) => ({
+        url: `/tasks/${arg._id}`,
+        method: 'PATCH',
+        body: { completed: !arg.completed },
+      }),
+      invalidatesTags: ['Tasks'],
+    }),
+  }),
+});
